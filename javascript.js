8ad1669faa6bfe158164e52a5fdd0915fbf4c002
wagdi alamri <35018106+Wagdi86@users.noreply.github.com>
2019-05-16 07:28:50 +0300

    first web

---
+++ javascript.js
@@ -0,0 +1,112 @@
+//var text = "meine erste ausgabe";
+//var text2 = ",Sehr gut!";
+//var num = 2;
+//var num2 = 50;
+//var sum = num * num2;
+//console.log(text + text2 + " " + sum);
+
+//............................
+
+//function wa() {
+//    console.log("mainFunction");
+//}
+//wa();
+
+//............................
+//parametern
+
+//function add(x, y){
+//    return x + y;
+//}
+//console.log(add(2, 3));
+
+//............................
+//parametern
+
+//function add(y, x){
+//    return y * x;
+//}
+//console.log(add(7,8));
+
+//............................
+//Methoden
+
+//var text = "Hello, mein Name ist Tine";
+//console.log(text.length);
+
+
+//............................
+//مكان العنصر
+//var text = "Hello, mein Name ist Tine";
+//console.log(text.length)
+//console.log(text.indexOf("Name"));
+
+//............................
+
+//var text = "Hello, mein Name ist Tine";
+//console.log(text.length)
+//console.log(text.indexOf("Name"));
+//console.log(text.slice(12, 25));
+//............................
+//*********************
+//Wichtig
+
+//var text = "Hello, mein Name ist Tine";
+//var text2 = ". wie geht es Ihnen";
+//var text3 = ". Ich bin Jemenitsch";
+//var text4 = ". Ich frue mich schone sehr";
+//console.log(text.length);
+//console.log(text.indexOf("Name"));
+//console.log(text.slice(12,25)); //write from 12 to 25
+//console.log(text.replace('Hallo',"Moin")); //replce text
+//console.log(text.toUpperCase());  //Capital
+//console.log(text.toLowerCase());  //small
+//console.log(text.concat(text2));    
+//console.log(text4.concat(text3));
+
+//............................
+//Method
+
+//var nummer = 32.65765;
+//var nummerToString = nummer.toString();
+//var nummerToFixed = nummer.toFixed(5);
+//var nummerToPrecision = nummer.toPrecision();
+//var addieren = (23 + 43).valueOf();
+//console.log(typeof nummerToString);
+
+//............................
+//Method
+
+//var nummer = 32.65765;
+//var nummerToString = nummer.toString();
+//var nummerToFixed = nummer.toFixed(5);
+//var nummerToPrecision = nummer.toPrecision();
+//var addieren = (3 + 4).valueOf();
+//console.log(addieren);
+
+
+//............................
+//Methods
+
+//var nummer = 32.65765;
+//var nummerToString = nummer.toString();
+//var nummerToFixed = nummer.toFixed(5);
+//var nummerToPrecision = nummer.toPrecision();
+//var addieren = (3 + 4).valueOf();
+//console.log(nummerToFixed);
+//............................
+
+
+
+//............................
+
+
+
+
+
+
+
+
+
+
+
